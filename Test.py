e9cd6356ae920d4c35623026394dbf709937d333
elput0amo <gmiradej@myuax.com>
2023-10-27 13:18:23 +0200

    .

---
+++ Test.py
@@ -0,0 +1,35 @@
+on= True 
+
+while on:
+    print(f"¿Que tipo de unidad quieres convertir?")
+    print("1. Divisas")
+    print("2. Longitud")
+    print ("3. Masa")
+    print( "4. Tiempo")
+    print("5. Velocidad")
+    print( "6. Temperatura")
+    print("7. Acabar con el programa")
+    conversion= input(f"")
+
+    if conversion== 1:
+        inicial=input()
+
+    if conversion==2:
+        inicial=input()
+
+    if conversion==3:
+        inicial=input()
+
+    if conversion==4:
+        inicial=input()
+
+    if conversion==5:
+        inicial=input()
+
+    if conversion==6:
+        inicial=input()
+        
+    if conversion==7:
+        break
+
+print(f"Gracias por utilizar nuestros servicios")
