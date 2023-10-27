bc676a4940492758ec7263cd68b24bba1ff1cbed
elenafernandezmac <145667817+elenafernandezmac@users.noreply.github.com>
2023-10-27 17:31:51 +0200

    Update biblioteca.py

--- biblioteca.py
+++ biblioteca.py
@@ -6,6 +6,36 @@ divisas = {
     "dinares" :3.35,
     "coronas":11.62,
     "francos":0.95
+}
 
+longitud = {
+    "metro": 1.
+    "millas": 0.000621371,
+    "yardas": 1.09361,
+    "pies": 3.28084,
+    "pulgadas": 39.3701
+}
 
+temperatura = {
+    "centigrados": 1,
+    farenheit = (centigrados *9/5) + 32,
+    kelvin = centigrados + 273,
+}
+
+masa = {
+    "kiogramos": 1,
+    "gramos": 1000,
+    "libras": 2.20462,
+    "onzas": 35.274,
+}
+
+tiempo = {
+    "horas": 1,
+    "minutos": 60,
+    "segundos": 3600,
+}
+
+velocidad = {
+    "kilometros/hora": 1,
+    "metros/segundo": 0.277778,
 }
