5f57ae6625a069e8d67fbb57d92596abe31784b5
mbvictor <85256180+mbvictor02@users.noreply.github.com>
2023-11-23 17:22:32 +0100

    statistics.py

--- statistics.py
+++ statistics.py
@@ -20,20 +20,3 @@ class Statistics:
 
     def calculate_variance(self, data):
         return np.var(data)
-
-# Usage example:
-stats = Statistics()
-
-values = stats.get_values()
-
-mean = stats.calculate_mean(values)
-print(f"Mean: {mean}")
-
-std_deviation = stats.calculate_standard_deviation(values)
-print(f"Standard Deviation: {std_deviation}")
-
-median = stats.calculate_median(values)
-print(f"Median: {median}")
-
-variance = stats.calculate_variance(values)
-print(f"Variance: {variance}")
